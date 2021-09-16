7809cd7b58f973c2feacfbf98d43ed6fce04916a
TravisHammondCSUS <47340315+Tiger767@users.noreply.github.com>
2021-09-16 14:15:29 -0700

    Update Hello.java

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("219236629 - Manuel Diaz.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("301137720 - This line was added by Travis Hammond.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
