f0d8951f281f48162d0457a29bd580e34f071938
manueldiazdev <manueldiazdev@gmail.com>
2021-09-19 19:28:29 -0700

    Updated Manuel Diaz line to fit format

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("219236629 - Manuel Diaz.");
+		System.out.println("219236629 - This line was added by Manuel Diaz.");
 		System.out.println("301137720 - This line was added by Travis Hammond.");
 		System.out.println("301231944 - This line was added by Ronaldo Ramirez.");
 		System.out.println("302323567 - This line was added by Lam Phan.");
