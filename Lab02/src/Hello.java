5c633e773a5da638a35478d72f8d669e9247210a
BMYasir <Bmyasirarafat@csus.edu>
2021-09-27 12:49:46 -0700

    Name Added by Yasir

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -11,7 +11,7 @@ public class Hello {
 		System.out.println("302323567 - This line was added by Lam Phan.");
 		System.out.println("302285139 - This line was added by Chris Allen.");
 		System.out.println("302268577 - This line was added by Devan McCall.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("302630341 - This line was added by B M Yasir Arafat.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 
